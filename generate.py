56826a6a85ab0206e1488fbeafac30e8d2b64abe
Lina Ihssane <cheekylin95@gmail.com>
2026-06-14 11:07:29 +0100

    random fun

---
+++ generate.py
@@ -0,0 +1,18 @@
+import random
+
+coin = random.choice(["Tails","Heads"])
+
+print(coin)
+
+alph = random.randint(1,1000)
+
+print(alph)
+
+
+lon = ["cards", "jack", "Queen"]
+random.shuffle(lon)
+
+for card in lon:
+    print(lon)
+
+    
