d6d82fbf2bc51a6b655319e1d5f35b2743691de2
Lina Ihssane <cheekylin95@gmail.com>
2026-06-15 21:19:05 +0100

    sorted

--- files.py
+++ files.py
@@ -1,14 +1,26 @@
-def creat():
-    name = input("What's your name??")
-    with open("names.txt" , 'a') as file:
-        file.write(f"{name}\n")
-    
 
-creat()
+# name = input("What's your name??")
+# with open("names.txt" , 'a') as file:
+# file.write(f"{name}\n")
 
+names = []
 with open("names.txt" , 'r') as file:
-     lines = file.readlines()
+    #  lines = file.readlines()
 
-for line in lines:
-    print("HEll000 to, ", line)
+# 1st method to remove the ectra new lines after reading the txt file ! -----
+# -------------------------------------------------------------------------
+# for line in lines:
+#     print("HEll000 to, ", line.rstrip())
     
+# 2nd method to do it , we removed the the 8th line before this
+#--------------------------------------------------------------------------
+    # for line in file:
+    #     print("HElloo to,", line.rstrip())
+
+# ----------------------------------------
+# to sort them out using names = [] now:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"HEllo to {name}")
