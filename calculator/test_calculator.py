3d6091c6fd317999f9e608b6eaa6c89542c27de8
Lina Ihssane <cheekylin95@gmail.com>
2026-06-15 15:17:24 +0100

    pytest passed

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -1,14 +1,8 @@
 from calculator import square
 
-def main():
-    test_sqea()
-
 def test_sqea():
-    try:
-        assert square(2) == 4
-        assert square(3) == 9
-
-    except AssertionError:
-        print("IT's true mate..........")
-if __name__ == "__main__":
-    main()
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
