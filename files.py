8d1547f27f23d373f20b4b0495332a4ca30e1f71
Lina Ihssane <cheekylin95@gmail.com>
2026-06-22 18:11:19 +0100

    split two pieces

--- files.py
+++ files.py
@@ -46,13 +46,13 @@ students = []
 
 with open("students.csv", 'r') as file:
     for line in file:
-        name,house = line.rstrip().split("/")
+        name, house = line.rstrip().split(",")
         student = {"name" : name,
                    "house" : house}
         students.append(student)
         
-def get_name(student):
-    return student["name"]
+def get_house(student):
+    return student["house"]
 
-for student in sorted(students , key=get_name):
+for student in sorted(students , key=get_house):
     print(f"{student ['name']} is in {student ['house']}  ")
